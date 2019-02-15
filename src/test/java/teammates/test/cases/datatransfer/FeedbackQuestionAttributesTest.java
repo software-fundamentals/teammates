0bac9195eeb1d8134e29bb251eb3c620b214fb1c
Josefin Nilsson <josefin.l.nilsson@gmail.com>
2019-02-15 14:02:21 +0100

    Add test for equals() function

--- src/test/java/teammates/test/cases/datatransfer/FeedbackQuestionAttributesTest.java
+++ src/test/java/teammates/test/cases/datatransfer/FeedbackQuestionAttributesTest.java
@@ -521,6 +521,20 @@ public class FeedbackQuestionAttributesTest extends BaseAttributesTest {
                 .build();
     }
 
+    @Test
+    public void testEquals() {
+        //Test that a FeedbackQuestionAttributes object equals itself
+        FeedbackQuestionAttributes questionAttributes = getNewFeedbackQuestionAttributes();
+        assertTrue(questionAttributes.equals(questionAttributes));
+
+        //Test that a FeedbackQuestionAttributes object doesn't equal null
+        assertFalse(questionAttributes.equals(null));
+
+        //Test that two objects that have all equal parameters are equal
+        FeedbackQuestionAttributes questionAttributes2 = getNewFeedbackQuestionAttributes();
+        assertTrue(questionAttributes.equals(questionAttributes2));
+    }
+
     private static class FeedbackQuestionAttributesWithModifiableTimestamp extends FeedbackQuestionAttributes {
 
         void setCreatedAt(Instant createdAt) {
