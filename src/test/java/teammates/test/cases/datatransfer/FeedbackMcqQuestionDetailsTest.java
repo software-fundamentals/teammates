77b8a43edd0bd071a3f1883eba75a5e48a4f0e15
williamwesterlund <william.e.westerlund@gmail.com>
2019-02-17 16:38:19 +0100

    Hotfix - covered branch count updated

--- src/test/java/teammates/test/cases/datatransfer/FeedbackMcqQuestionDetailsTest.java
+++ src/test/java/teammates/test/cases/datatransfer/FeedbackMcqQuestionDetailsTest.java
@@ -293,8 +293,8 @@ public class FeedbackMcqQuestionDetailsTest extends BaseTestCase {
         assertEquals(1, errors.size());
         assertEquals(Const.FeedbackQuestion.MCQ_ERROR_NOT_ENOUGH_CHOICES
                 + Const.FeedbackQuestion.MCQ_MIN_NUM_OF_CHOICES + ".", errors.get(0));
+                                                                                            // Before: Covered branches: 3 of 9
 
-        mcqDetails.testValidateQuestionDetailsCoverage();   // Covered branches: 3 of 9
     }
 
     @Test
@@ -411,5 +411,6 @@ public class FeedbackMcqQuestionDetailsTest extends BaseTestCase {
         assertFalse(mcqDetails.hasAssignedWeights());
         assertTrue(mcqDetails.getMcqWeights().isEmpty());
         assertEquals(0.0, mcqDetails.getMcqOtherWeight());
+        mcqDetails.testValidateQuestionDetailsCoverage(); // Covered branches: 9 of 9
     }
 }
